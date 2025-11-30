13746220748b79eb582a544d403cdb09ed91100d
Shine <zero777960@gmail.com>
2025-11-30 23:57:10 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      home: Scaffold(
+        body: Row(
+          children: [
+            Icon(Icons.star),
+            Icon(Icons.favorite),
+            Icon(Icons.thumb_up),
+          ],
+        ),
+      )
+    );
+    
+  }
+}
